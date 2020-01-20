f3279924c809858564a7cc4a2f3870fca6b943a1
Fredrik93 <fredrikullman93@gmail.com>
2020-01-20 15:24:08 +0100

    add comment

--- exerc_1_2.c
+++ exerc_1_2.c
@@ -3,6 +3,8 @@
 
 int main()
 {
+
+    //everytime theres a space it counts it as a new word, so "?" would count as a word.. perhaps theres a nice fix for all these edge - cases
     char letters[MAX];
     int counter = 1;
     fgets(letters, MAX, stdin);
