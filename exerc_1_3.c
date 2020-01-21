351a610b2ba56f983c668eb6d80aeef19a836421
Fredrik93 <fredrikullman93@gmail.com>
2020-01-21 14:00:49 +0100

    algorithm done

--- exerc_1_3.c
+++ exerc_1_3.c
@@ -13,26 +13,128 @@ Demonstration code: [<Ass code 1-4> <abc>] Important , No code no exercise point
 
 #include <stdio.h>
 #define MAX 50
-
 char word[MAX];
 
 int main()
 {
-    printf("Enter a word to encrypt it : \n");
-    fgets(word, MAX, stdin);
-    printf("word is : %s\n", word);
 
-    for (int i = 0; i < 10; i++)
+    printf("Enter a word to encrypt it : ");
+    while (1)
     {
-        if (word[i] == 'a')
-        {
-            word[i] = 'n';
-        }
-        else if (word[i] == 'n')
-        {
-            word[i] = 'a';
-        }
-    }
 
-    printf("Encrypted Word is : %s\n", word);
+        fgets(word, MAX, stdin);
+        printf("word is : %s\n", word);
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (word[i] == 'a')
+            {
+                word[i] = 'n';
+            }
+            else if (word[i] == 'n')
+            {
+                word[i] = 'a';
+            }
+            if (word[i] == 'b')
+            {
+                word[i] = 'o';
+            }
+            else if (word[i] == 'o')
+            {
+                word[i] = 'b';
+            }
+            if (word[i] == 'c')
+            {
+                word[i] = 'p';
+            }
+            else if (word[i] == 'p')
+            {
+                word[i] = 'c';
+            }
+
+            if (word[i] == 'd')
+            {
+                word[i] = 'q';
+            }
+            else if (word[i] == 'q')
+            {
+                word[i] = 'd';
+            }
+            if (word[i] == 'e')
+            {
+                word[i] = 'r';
+            }
+            else if (word[i] == 'r')
+            {
+                word[i] = 'e';
+            }
+            if (word[i] == 'f')
+            {
+                word[i] = 's';
+            }
+            else if (word[i] == 's')
+            {
+                word[i] = 'f';
+            }
+            if (word[i] == 'g')
+            {
+                word[i] = 't';
+            }
+            else if (word[i] == 't')
+            {
+                word[i] = 'g';
+            }
+            if (word[i] == 'h')
+            {
+                word[i] = 'u';
+            }
+            else if (word[i] == 'u')
+            {
+                word[i] = 'h';
+            }
+            if (word[i] == 'i')
+            {
+                word[i] = 'v';
+            }
+            else if (word[i] == 'v')
+            {
+                word[i] = 'i';
+            }
+            if (word[i] == 'j')
+            {
+                word[i] = 'w';
+            }
+            else if (word[i] == 'w')
+            {
+                word[i] = 'j';
+            }
+            if (word[i] == 'k')
+            {
+                word[i] = 'x';
+            }
+            else if (word[i] == 'x')
+            {
+                word[i] = 'k';
+            }
+            if (word[i] == 'l')
+            {
+                word[i] = 'y';
+            }
+            else if (word[i] == 'y')
+            {
+                word[i] = 'l';
+            }
+            if (word[i] == 'm')
+            {
+                word[i] = 'z';
+            }
+            else if (word[i] == 'z')
+            {
+                word[i] = 'm';
+            }
+        }
+
+        printf("Encrypted Word is : %s\n", word);
+        printf("Try another word : ");
+    }
 }
