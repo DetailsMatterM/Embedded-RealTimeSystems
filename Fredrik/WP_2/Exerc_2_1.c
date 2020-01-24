d8211dd6c0c3069f504f9eb8ebf6dabe5fc1bb76
Fredrik93 <fredrikullman93@gmail.com>
2020-01-24 09:24:35 +0100

    exerc updated

--- Fredrik/WP_2/Exerc_2_1.c
+++ Fredrik/WP_2/Exerc_2_1.c
@@ -13,17 +13,28 @@ Demonstration code: [<Ass code 1-4> <abc>] Important , No code no exercise point
 #include <stdio.h>
 #include <string.h>
 #define MAX 20
+char word[MAX];
+void copyString()
+{
+    char savedWord[MAX];
+    savedWord[MAX];
+    for (int i = 0; i < MAX + 1; i++)
+    {
+        savedWord[i] = word[i];
+    }
+
+    printf("printed from copyString : %s", savedWord);
+}
 
 int main()
 {
-    char word[MAX];
-    char saved[20];
-    char savedInput[MAX];
+
+    char saved[MAX];
     printf("Type something \n");
     fgets(word, MAX, stdin);
 
     // a ) copy the string to another string
     strcpy(saved, word);
-
-    printf("%s", saved);
+    printf("printed from strcpy() : %s", saved);
+    copyString();
 }
