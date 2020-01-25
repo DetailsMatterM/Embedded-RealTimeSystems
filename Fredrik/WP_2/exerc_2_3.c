08367270efe3603ca99d938faec5b36400816c0c
Fredrik93 <fredrikullman93@gmail.com>
2020-01-25 08:44:08 +0100

    name change of files

---
+++ Fredrik/WP_2/exerc_2_3.c
@@ -0,0 +1,12 @@
+/*====================================
+File name: exerc_2_3.c
+Date: 2020-02-07
+Group nr 7
+Members that contribute to the solutions
+Moriz Denke 
+Mikael Köse Jansson 
+Fredrik Ullman
+Member not present at demonstration time:
+Yyyyyy yyyy
+Demonstration code: [<Ass code 1-4> <abc>] Important , No code no exercise points !
+======================================*/
