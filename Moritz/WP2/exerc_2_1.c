798bfecf0e8e873660c70c63c16c2ef98b5d9bf2
DetailsMatterM <moritz.denke@outlook.com>
2020-01-26 18:05:57 +0100

    2_1done

--- Moritz/WP2/exerc_2_1.c
+++ Moritz/WP2/exerc_2_1.c
@@ -4,18 +4,16 @@
 #define MAX 21
 
 void copyString(char * input, char * output);
-void clearstdin(void);
 
 int main(char textToCopy [20]) {
     fflush(stdin);
     char textSource [MAX];
     char textCopy [MAX];
-    char textCopyCustom [50];
+    char textCopyCustom [MAX];
     char * dest = textCopy;
     char * src = textSource;
     char * dest2 = textCopyCustom;
     fgets(textSource, MAX, stdin);
-    //scanf("%20s", textSource);
     strcpy(dest, src);
 
     puts("\nThis is the copied string using strcpy:");
@@ -31,7 +29,6 @@ int main(char textToCopy [20]) {
 
 //improve (since int byte is bigger than char byte)
 void copyString(char * input, char * output){
-    char i = 0;
     while(*input != '\0'){
         *output = *input;
         output++;
