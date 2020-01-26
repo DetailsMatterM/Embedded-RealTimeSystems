2e2ab6b25bccd639fc76380ba2c37529763613ad
Fredrik93 <fredrikullman93@gmail.com>
2020-01-26 14:31:36 +0100

    compare using library function

--- Fredrik/WP_2/exerc_2_3.c
+++ Fredrik/WP_2/exerc_2_3.c
@@ -12,16 +12,32 @@ Demonstration code: [<Ass code 1-4> <abc>] Important , No code no exercise point
 ======================================*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX 50
+
+
 
 int main(int argc, char *argv[])
 {
 
     if (argc != 3)
     {
-        printf("Type : %s firstword secondword \n", argv[0]);
+        printf("Type : %s firstWord secondWord\n", argv[0]);
         return 1;
     }
-    printf("First word : %s , Second word : %s \n", argv[1], argv[2]);
+    printf(" First word  : %s\n Second word : %s\n", argv[1], argv[2]);
+
+    int compare = strcmp(argv[1], argv[2]);
+
+    if (compare == 0)
+    {
+        printf("%s and %s\n ", argv[1], argv[2]);
+        printf(" Words are identical ! ");
+    }
+    else
+    {
+        printf("Words are not identical");
+    }
+
     return 0;
 }
