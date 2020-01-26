f8427778dae936e5580acca6b52023c99a7a0987
DetailsMatterM <moritz.denke@outlook.com>
2020-01-26 16:25:20 +0100

    2_1

--- Moritz/WP2/exerc_2_1.c
+++ Moritz/WP2/exerc_2_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 20
+#define MAX 21
 
 void copyString(char * input, char * output);
 void clearstdin(void);
@@ -10,18 +10,19 @@ int main(char textToCopy [20]) {
     fflush(stdin);
     char textSource [MAX];
     char textCopy [MAX];
-    char textCopyCustom [MAX];
+    char textCopyCustom [50];
     char * dest = textCopy;
     char * src = textSource;
     char * dest2 = textCopyCustom;
-    scanf("%20s", textSource);
+    fgets(textSource, MAX, stdin);
+    //scanf("%20s", textSource);
     strcpy(dest, src);
-    
+
     puts("\nThis is the copied string using strcpy:");
     printf("\n%s", textCopy);
     puts("\n==========================================");
 
-    
+
     puts("\nThis is the copied string using copyString:");
     copyString(src, dest2);
     printf("\n%s", textCopyCustom);
@@ -31,10 +32,10 @@ int main(char textToCopy [20]) {
 //improve (since int byte is bigger than char byte)
 void copyString(char * input, char * output){
     char i = 0;
-        while(*input != '\0'){
-            *output = *input;
-            output++;
-            input++;
-        }
+    while(*input != '\0'){
+        *output = *input;
+        output++;
+        input++;
+    }
     *output = '\0';
 }
