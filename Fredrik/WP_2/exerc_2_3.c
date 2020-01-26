fd9a8ab0fefc4c6aebbc629a6727e8cfa929035c
Fredrik93 <fredrikullman93@gmail.com>
2020-01-26 15:41:34 +0100

    add option argument

--- Fredrik/WP_2/exerc_2_3.c
+++ Fredrik/WP_2/exerc_2_3.c
@@ -18,44 +18,59 @@ Demonstration code: [<Ass code 1-4> <abc>] Important , No code no exercise point
 int main(int argc, char *argv[])
 {
 
-    if (argc != 3)
+    if (argc != 4)
     {
-        printf("Type : %s firstWord secondWord\n", argv[0]);
+        printf("Type : %s firstWord secondWord and option 1 or 2\n", argv[0]);
         return 1;
     }
-    printf(" First word  : %s\n Second word : %s\n", argv[1], argv[2]);
+    printf(" First word  : %s\n Second word : %s\n option : %s \n", argv[1], argv[2], argv[3]);
+
+    char option = argv[3][0];
 
     // compare using the strcmp function
-    // int compare = strcmp(argv[1], argv[2]);
-    // if (compare == 0)
-    // {
-    //     printf("%s and %s\n ", argv[1], argv[2]);
-    //     printf(" Words are identical ! ");
-    // }
-    //compare without use of strcmp function
-    printf("%s %s\n", argv[1], argv[2]);
-
-    int allCorrect = 1;
-
-    for (int i = 0; i < strlen(argv[1]) || i < strlen(argv[2]); i++)
+    if (option == '1')
     {
-        if (argv[1][i] == argv[2][i])
+        int compare = strcmp(argv[1], argv[2]);
+        if (compare == 0)
         {
-            printf("Letter %d identical \n", i);
+            printf("%s and %s\n ", argv[1], argv[2]);
+            printf(" Words are identical ! ");
         }
         else
         {
-            printf(" Letter %d Words are not identical \n", i);
-            allCorrect = 0;
+            printf(" Words are not identical ! ");
         }
     }
-    if (allCorrect == 1)
+    //compare without use of strcmp function
+    else if (option == '2')
     {
-        printf("Awesome words are identical\ n");
+
+        int allCorrect = 1;
+
+        for (int i = 0; i < strlen(argv[1]) || i < strlen(argv[2]); i++)
+        {
+            if (argv[1][i] == argv[2][i])
+            {
+                printf("Letter %d identical \n", i);
+            }
+            else
+            {
+                printf(" Letter %d Words are not identical \n", i);
+                allCorrect = 0;
+            }
+        }
+        if (allCorrect == 1)
+        {
+            printf("Words are identical\ n");
+        }
+        else if (allCorrect == 0)
+        {
+            printf("Sorry, not all letters were identical \n");
+        }
     }
-    else if (allCorrect == 0)
+    else
     {
-        printf("Sorry, not all letters were identical \n");
+        printf("Incorrect option , 1 or 2 ");
     }
 
     return 0;
