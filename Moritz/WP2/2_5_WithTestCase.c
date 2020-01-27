96648a69f7b8155cd2eade389b0c661737e17d73
DetailsMatterM <moritz.denke@outlook.com>
2020-01-27 12:35:04 +0100

    2_5 done

---
+++ Moritz/WP2/2_5_WithTestCase.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 4
+#define MAXNUMBER 3
+
+void create_random(int *tab);
+void count_frequency(int *tab, int *freq);
+void draw_historgram(int *freq);
+
+int main() {
+    int table [MAX], n;
+    int frequency[MAXNUMBER];
+
+    int *tab = table;
+    int *freq = frequency;
+
+    create_random(tab);
+    for(int p = 0; p < MAX; p++) {
+        printf("\n Table: %d", table[p]);
+    }
+    count_frequency(tab, freq);
+    for(int y = 0; y < MAX; y++) {
+        printf("\n Frequency: %d", frequency[y]);
+    }
+    draw_historgram(freq);
+    return 0;
+}
+
+void create_random(int *tab) {
+    srand(time(NULL));
+    for(int i = 0; i < MAX; i++) {
+        *tab = rand() % (MAXNUMBER + 1);
+        tab++;
+    }
+}
+
+void count_frequency(int *tab, int *freq) {
+    int * tabTemp = tab;
+    int n = 0;
+    for(int i = 0; i <= MAXNUMBER; i++) {
+        for(int y = 0; y < MAX; y++) {
+            if (i == *tab){
+                n++;
+            }
+            tab++;
+        }
+        *freq = n;
+        freq++;
+        n = 0;
+        tab = tabTemp;
+    }
+}
+
+void draw_historgram(int *freq) {
+     for(int i = 0; i <= MAXNUMBER; i++) {
+        if(*freq != 0) {
+            if(i < 10) {
+                printf("\n%d  ", i);
+            } else {
+                printf("\n%d ", i);
+            }
+            
+            for(int y = 0; y < *freq; y++) {
+                printf("%s", "x");
+            }
+        }
+        freq++;
+    }
+}
