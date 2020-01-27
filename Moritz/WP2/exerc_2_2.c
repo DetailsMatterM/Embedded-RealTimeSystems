776176e40ff003e0cf28bc9c6e6cdbab628f2932
DetailsMatterM <moritz.denke@outlook.com>
2020-01-27 10:37:14 +0100

    2_2

---
+++ Moritz/WP2/exerc_2_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 50
+int main() {
+    srand(time(NULL));
+    int random [MAX];
+    int * pointer = random;
+    int i;
+    for(i = 0; i < MAX; i++) {
+        random[i] = (rand() % (99 - 1 + 1)) + 1;
+    }
+    printf("\n%s", "The value of the label array(address) is: ");
+    printf("%pn", &random);
+    printf("\n%s", "First integer in the array is (array[0]) : ");
+    printf("%d", random[0]);
+    printf("\n%s", "The size of an integer (number of bytes) is : ");
+    printf("%zu", sizeof(i));
+    printf("\n%s", "The size of the whole array is: ");
+    printf("%zu", sizeof(random));
+
+    int convert;
+    for(int y = 0; y < MAX; y++) {
+        convert = *pointer; 
+        printf("\n Int: %d", convert);
+        printf(" Double: %f", (double)convert);
+        pointer++;
+    }
+
+    return 0;
+}
