8b9509168c2e21b13ea5c0ed2d13f3de3ad7c2af
DetailsMatterM <moritz.denke@outlook.com>
2020-02-06 19:22:03 +0100

    problem

--- Moritz/WP3/exerc_3_2.c
+++ Moritz/WP3/exerc_3_2.c
@@ -6,15 +6,26 @@ void sort (int number, int tab[]);
 
 int main () {
     int test [] = { 1,2,34,5,67,3,23,12,13,10};
-    int a = search_number(TESTNUMBER, test, (sizeof(test)/sizeof(test[0])));
-    printf("%d", a);
+    int size = (sizeof(test)/sizeof(test[0]));
+    int a = search_number(TESTNUMBER, test, size);
+    printf("%d\n", a);
 
     //implement for loop. Arguments are number standing for the intex from which we are still searching.
+    for(int j = 0; j < size; j++) {
+        printf("%d,", test[j]);
+    }
+    printf("\n");
+    for (int i = 0; i < size; i++) {
+        sort(i, test);
+    }
+    for(int j = 0; j < size; j++) {
+        printf("%d,", test[j]);
+    }
 }
 
 int search_number (int number, int tab[], int size) {
     for (int i = 0; i < size; i++) {
-        if(tab[i] == number) {
+        if (tab[i] == number) {
             return i;
         }
     }
@@ -30,5 +41,9 @@ void sort (int number, int tab[]) {
            minIndex = i;
         }
     }
-
+    if (tempmin != MAXVALUE) {
+        tab[minIndex] = tab[number];
+        tab[number] = tempmin;
+    }
+    
 }
