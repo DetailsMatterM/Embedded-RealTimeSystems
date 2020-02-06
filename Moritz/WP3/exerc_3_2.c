cf623919c7c93327e5cbb83b58f1aa673f01f3d1
DetailsMatterM <moritz.denke@outlook.com>
2020-02-06 18:51:40 +0100

    3_2_progress

--- Moritz/WP3/exerc_3_2.c
+++ Moritz/WP3/exerc_3_2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#define testNumber 23
-
+#define TESTNUMBER 23
+#define MAXVALUE 2147483647
 int search_number (int number, int tab [], int size);
+void sort (int number, int tab[]);
 
 int main () {
     int test [] = { 1,2,34,5,67,3,23,12,13,10};
-    int a = search_number(testNumber, test, (sizeof(test)/sizeof(test[0])));
+    int a = search_number(TESTNUMBER, test, (sizeof(test)/sizeof(test[0])));
     printf("%d", a);
+
+    //implement for loop. Arguments are number standing for the intex from which we are still searching.
 }
 
 int search_number (int number, int tab[], int size) {
@@ -16,4 +19,16 @@ int search_number (int number, int tab[], int size) {
         }
     }
     return -1;
+}
+
+void sort (int number, int tab[]) {
+    int tempmin = MAXVALUE;
+    int minIndex;
+    for (int i = number; i < (sizeof(tab)/sizeof(tab[0])); i++) {
+        if (tab[i] <= tempmin) {
+           tempmin = tab[i]; 
+           minIndex = i;
+        }
+    }
+
 }
