682390ca397966e7ab4970fc0e0bd6c32befa526
DetailsMatterM <moritz.denke@outlook.com>
2020-02-09 18:59:33 +0100

    3_2 done

--- Moritz/WP3/exerc_3_2.c
+++ Moritz/WP3/exerc_3_2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#define TESTNUMBER 23
-#define MAXVALUE 2147483647
+#define TESTNUMBER 34
 int search_number (int number, int tab [], int size);
 void sort (int number, int tab[]);
 
@@ -8,18 +7,20 @@ int main () {
     int test [] = { 1,2,34,5,67,3,23,12,13,10};
     int size = (sizeof(test)/sizeof(test[0]));
     int a = search_number(TESTNUMBER, test, size);
-    printf("%d\n", a);
-
-    //implement for loop. Arguments are number standing for the intex from which we are still searching.
-    for(int j = 0; j < size; j++) {
-        printf("%d,", test[j]);
+    if (a != -1) {
+        printf("Number %d is found at index %d\n", TESTNUMBER, a);
+    } else {
+        printf("Number is not part of the array\n");
     }
-    printf("\n");
-    for (int i = 0; i < size; i++) {
-        sort(i, test);
-    }
+    
+    printf("Unsorted Array:\n");
     for(int j = 0; j < size; j++) {
-        printf("%d,", test[j]);
+        printf("%d|", test[j]);
+    }
+    printf("\nSorted Array\n");
+    sort(size, test);   
+    for (int j = 0; j < size; j++) {
+        printf("%d|", test[j]);
     }
 }
 
@@ -33,17 +34,14 @@ int search_number (int number, int tab[], int size) {
 }
 
 void sort (int number, int tab[]) {
-    int tempmin = MAXVALUE;
-    int minIndex;
-    for (int i = number; i < (sizeof(tab)/sizeof(tab[0])); i++) {
-        if (tab[i] <= tempmin) {
-           tempmin = tab[i]; 
-           minIndex = i;
+    int i, j, temp;
+    for (i = 0; i < number; i++) {
+        for ( j = 0; j < number - i - 1; j++) {
+            if (tab[j] > tab[j + 1]) {
+                temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
         }
     }
-    if (tempmin != MAXVALUE) {
-        tab[minIndex] = tab[number];
-        tab[number] = tempmin;
-    }
-    
 }
