5d299cda67d69da7c88e86526db7d316922bfdd0
DetailsMatterM <moritz.denke@outlook.com>
2020-02-12 09:44:32 +0100

    doneNearlyPerfect

--- Moritz/WP3/exerc_3_4.c
+++ Moritz/WP3/exerc_3_4.c
@@ -9,6 +9,19 @@ Demonstration code: [<Ass code 1-3> <abc>] Important !
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+/* ====================================
+File name: exerc_3_4.c (or cpp)
+Date: 2019-mm-dd
+Group Number:xxxx
+Members that contributed:
+
+Demonstration code: [<Ass code 1-3> <abc>] Important !
+====================================== */
+
+#include <stdlib.h>
+#include <stdio.h>
 
 
 // -----Typedefs -------
@@ -24,61 +37,61 @@ void write_new_file (PERSON *inrecord); //Creats a file and write a first record
 void printfile (void); // print out all persons in the file
 void search_by_firstname (char *name);// print out person if in list
 void append_file (PERSON *inrecord);// appends a new person to the file
-int createNewFile();
 
 int main (void) {
-    
+
     int endCheck = 0;
     while (endCheck == 0) {
         printf("Welcome to the person register. How may we be of service?\nChose one of the following options"
-           "by typing the according number:\n"
-           "1 Create a new and delete the old file.\n"
-           "2 Add a new person to the file.\n"
-           "3 Search for a person in the file .\n"
-           "4 Print out all in the file.\n"
-           "5 Exit the program.\n");
+               "by typing the according number:\n"
+               "1 Create a new and delete the old file.\n"
+               "2 Add a new person to the file.\n"
+               "3 Search for a person in the file .\n"
+               "4 Print out all in the file.\n"
+               "5 Exit the program.\n");
 
         int input;
         scanf ("%d", &input);
         switch(input) {
-        case 1:
-            printf("Your file will be generated.\n");
-            PERSON dummyPerson;
-            PERSON *dummy = &dummyPerson;
-            write_new_file(dummy);
-            break;
-        case 2:
-            fflush(stdin);
-            printf("Please enter a first name (max length 19)\n");
-            PERSON person;
-            PERSON *pers = &person;
-            fgets(pers -> firstname, 20, stdin);
+            case 1:
+                printf("Your file will be generated.\n");
+                PERSON dummyPerson;
+                PERSON *dummy = &dummyPerson;
+                write_new_file(dummy);
+                break;
+            case 2:
+                fflush(stdin);
+                printf("Please enter a first name (max length 19)\n");
+                PERSON person;
+                PERSON *pers = &person;
+                fgets(pers -> firstname, 20, stdin);
 
-            printf("Please enter a last name (max length 19)\n");
-            fgets(pers-> famnamne, 20, stdin);
-            printf("Please enter a personnumer\n");
-            fgets(pers -> pers_number, 13, stdin);
-            append_file(pers);
-            break;
-        case 3:
-            fflush(stdin);
-            printf("Please enter the first name (max length 19) that you want to search for\n");
-            char firstname[20];
-            fgets(firstname, 20, stdin);
-            char *fname = &firstname;
-            search_by_firstname(fname);
-            break;
-        case 4:
-            printfile();
-            break;
-        case 5:
-            endCheck = 1;
-            printf("Good Bye!");
-            break;
+                printf("Please enter a last name (max length 19)\n");
+                fgets(pers-> famnamne, 20, stdin);
+                printf("Please enter a personnumer\n");
+                fgets(pers -> pers_number, 13, stdin);
+                fflush(stdin);
+                append_file(pers);
+                break;
+            case 3:
+                fflush(stdin);
+                printf("Please enter the first name (max length 19) that you want to search for\n");
+                char firstname[20];
+                fgets(firstname, 20, stdin);
+                char *fname = firstname;
+                search_by_firstname(fname);
+                fflush(stdin);
+                break;
+            case 4:
+                printfile();
+                break;
+            case 5:
+                endCheck = 1;
+                printf("Good Bye!");
+                break;
         }
     }
-    
-    //PERSON ppost;
+
     return(0);
 }
 
@@ -93,7 +106,7 @@ void append_file (PERSON *inrecord) {
     char filename[] = {"personfile.dat"};
     FILE *fileptr = fopen(filename,"ab+");
     fwrite(inrecord, sizeof(PERSON), 1, fileptr);
-    fclose(fileptr);   
+    fclose(fileptr);
 }
 
 void search_by_firstname (char *name) {
@@ -121,17 +134,18 @@ void printfile (void) {
     PERSON *pers = &person;
     int n = 1;
     char temp [13];
+    printf("People in the register:\n");
     while(fileptr != NULL){
         fseek (fileptr, n * sizeof(PERSON), SEEK_SET);
         fread (&person, sizeof(PERSON), 1, fileptr);
         int a = strcmp(temp, person.pers_number);
         if(a != 0) {
-            printf("%s%s%s\n", pers->firstname, pers->famnamne, pers->pers_number);
+            printf("\n%s%s%s\n", pers->firstname, pers->famnamne, pers->pers_number);
             strcpy(temp, pers->pers_number);
             n++;
         } else {
+            fclose(fileptr);
             break;
         }
     }
-   fclose(fileptr);
 }
