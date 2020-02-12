b209d8cd550e340543dfbf46487820d886eb9243
Fredrik93 <fredrikullman93@gmail.com>
2020-02-12 11:25:10 +0100

    exerc 3_2

--- Fredrik/WP_3/exerc_3_1.c
+++ Fredrik/WP_3/exerc_3_1.c
@@ -11,17 +11,14 @@ enum DIRECTION
     W
 };
 
-struct my_struct
-{
-    int num;
-};
-
 typedef struct
 {
     int xpos;
     int ypos;
     enum DIRECTION dir;
 } ROBOT;
+
+void move(ROBOT *ptr);
 
 int main()
 {
@@ -35,17 +32,49 @@ int main()
     getchar();
     printf("Enter a string of commands : \n");
     fgets(command, 20, stdin);
-    printf("X pos is %d y pos is %d \n", robot.xpos, robot.ypos);
+    //printf("X pos is %d y pos is %d \n", robot.xpos, robot.ypos);
     //using a struct pointer
     ROBOT *structPointer = &robot;
-    structPointer->dir = 'N';
+    structPointer->dir = N;
+
     printf("X struct pos is %d y struct pos is %d  and the direction is %c, commands are : %s\n",
            structPointer->xpos, structPointer->ypos, structPointer->dir, command);
+    for (int i = 0; i < 20; i++)
+    {
+        if (command[i] == 'm')
+        {
+            move(structPointer);
+        }
+        else
+        {
+            turn(structPointer);
+        }
+    }
+    printf("\n X coordinate is %d and y coordinate is %d", structPointer->xpos, structPointer->ypos);
 
     return 0;
 }
 
-void move(struct my_struct *my_ptr)
+void turn(ROBOT *ptr)
 {
-    printf("\nPost Increment: %d", my_ptr->num++);
+    ptr->dir++;
+}
+void move(ROBOT *ptr)
+{
+    if (ptr->dir == N && ptr->ypos != 99)
+    {
+        ptr->ypos++;
+    }
+    else if (ptr->dir == O && ptr->xpos != 99)
+    {
+        ptr->xpos++;
+    }
+    else if (ptr->dir == S && ptr->ypos != 0)
+    {
+        ptr->ypos--;
+    }
+    else if (ptr->dir == W && ptr->xpos != 0)
+    {
+        ptr->xpos--;
+    }
 }
--- Fredrik/WP_3/exerc_3_2.c
+++ Fredrik/WP_3/exerc_3_2.c
@@ -2,13 +2,35 @@
 #include <stdio.h>
 
 int search_number(int number, int tab[], int size);
+void bubbleSort(int tab[], int number);
+void printArray(int tab[], int size);
+
+int main()
+{
+    int number;
+    printf("Enter a number : ");
+    scanf("%d", &number);
+    int test[MAX] = {1, 2, 34, 5, 67, 3, 23, 12, 13, 10};
+    int size = (sizeof(test) / sizeof(test[0]));
+
+    search_number(number, test, MAX);
+
+    printf("Unsorted Array:\n");
+    printArray(test, size);
+
+    printf("\nSorted Array\n");
+    bubbleSort(test, size);
+    printArray(test, size);
+
+    return 0;
+}
 
 int search_number(int number, int tab[], int size)
 {
     int flag = 0;
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < size; i++)
     {
-        if (number == tab[i])
+        if (tab[i] == number)
         {
             printf("number : %d exists in array!  index is %d \n", number, i);
             flag = 1;
@@ -22,23 +44,21 @@ int search_number(int number, int tab[], int size)
     }
 }
 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-// A function to implement bubble sort
 void bubbleSort(int tab[], int number)
 {
-    int i, j;
-    for (i = 0; i < number - 1; i++)
-
-        // Last i elements are already in place
+    int i, j, temp;
+    for (i = 0; i < number; i++)
+    {
         for (j = 0; j < number - i - 1; j++)
+        {
             if (tab[j] > tab[j + 1])
-                swap(&tab[j], &tab[j + 1]);
+            {
+                temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
 }
 
 /* Function to print an array */
@@ -46,21 +66,6 @@ void printArray(int tab[], int size)
 {
     int i;
     for (i = 0; i < size; i++)
-        printf("%d ", tab[i]);
+        printf("%d|", tab[i]);
     printf("\n");
-}
-
-int main()
-{
-    int number;
-    printf("Enter a number : ");
-    scanf("%d", &number);
-
-    int test[MAX] = {1, 2, 34, 5, 67, 3, 23, 12, 13, 10};
-    search_number(number, test, MAX);
-
-    bubbleSort(test, number);
-    printf("Sorted array: \n");
-    printArray(test, number);
-    return 0;
 }
