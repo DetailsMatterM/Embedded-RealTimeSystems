31a52c4160a9e5f7b7e01e6fa8f684b4741c6f01
DetailsMatterM <moritz.denke@outlook.com>
2020-02-13 13:36:33 +0100

    improvemore

--- Moritz/WP3/exerc_3_4.c
+++ Moritz/WP3/exerc_3_4.c
@@ -36,6 +36,7 @@ PERSON input_record (void); // Reads in a person record.
 void write_new_file (PERSON *inrecord); //Creats a file and write a first record
 void printfile (void); // print out all persons in the file
 void search_by_firstname (char *name);// print out person if in list
+void search_by_lastname (char *name);
 void append_file (PERSON *inrecord);// appends a new person to the file
 
 int main (void) {
@@ -46,11 +47,11 @@ int main (void) {
                "by typing the according number:\n"
                "1 Create a new and delete the old file.\n"
                "2 Add a new person to the file.\n"
-               "3 Search for a person in the file .\n"
+               "3 Search for a person in the file.\n"
                "4 Print out all in the file.\n"
                "5 Exit the program.\n");
 
-        int input;
+        int input = 0;
         scanf ("%d", &input);
         fflush(stdin);
         switch(input) {
@@ -78,11 +79,25 @@ int main (void) {
                 break;
             case 3:
                 fflush(stdin);
-                printf("Please enter the first name (max length 19) that you want to search for\n");
-                char firstname[20];
-                fgets(firstname, 20, stdin);
-                char *fname = firstname;
-                search_by_firstname(fname);
+                int choice = 0;
+                printf("Enter 1 for firstname or 2 for lastname.\n");
+                scanf("%d", &choice);
+                if (choice == 1) {
+                    fflush(stdin);
+                    printf("Please enter the first name (max length 19) that you want to search for\n");
+                    char firstname[20];
+                    fgets(firstname, 20, stdin);
+                    char *fname = firstname;
+                    search_by_firstname(fname);
+                } else if (choice == 2) {
+                    fflush(stdin);
+                    printf("Please enter the last name (max length 19) that you want to search for\n");
+                    char lastname[20];
+                    fgets(lastname, 20, stdin);
+                    char *fname = lastname;
+                    search_by_lastname(fname);
+                }
+                
                 fflush(stdin);
                 break;
             case 4:
@@ -95,7 +110,6 @@ int main (void) {
                 break;
             default:
                 printf("Please enter valid input!\n");
-                fflush(stdin);
                 break;
         }
     }
@@ -131,6 +145,28 @@ void search_by_firstname (char *name) {
         fseek (fileptr, n * sizeof(PERSON), SEEK_SET);
         fread (&person, sizeof(PERSON), 1, fileptr);
         if ((strcmp(person.firstname, name)) == 0) {
+            printf("\n%s%s%s\n", pers->firstname, pers->famnamne, pers->pers_number);
+        }
+        n++;
+        counter = counter + sizeof(person);
+    }
+    fclose(fileptr);
+}
+
+void search_by_lastname (char *name) {
+    char filename[] = {"personfile.dat"};
+    FILE *fileptr = fopen(filename,"rb");
+    PERSON person;
+    PERSON *pers = &person;
+    int n = 0;
+    fseek (fileptr, 0 , SEEK_END);
+    long size = ftell (fileptr) ;
+    rewind (fileptr);
+    double counter = 0;
+    while (counter < size) {
+        fseek (fileptr, n * sizeof(PERSON), SEEK_SET);
+        fread (&person, sizeof(PERSON), 1, fileptr);
+        if ((strcmp(person.famnamne, name)) == 0) {
             printf("\n%s%s%s\n", pers->firstname, pers->famnamne, pers->pers_number);
         }
         n++;
