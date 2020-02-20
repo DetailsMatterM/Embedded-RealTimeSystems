b89c8d0634927a7e63860d60d6cdeb4ecbb9cfd2
DetailsMatterM <moritz.denke@outlook.com>
2020-02-20 13:04:08 +0100

    submission

---
+++ Submission/WP4/code.c
@@ -0,0 +1,37 @@
+/* ====================================
+File name: code.c
+Date: 2019-02-20
+Group Number:07
+Members that contributed:
+Fredrik Ullman
+Mikael Köse Jansson
+Moritz Denke
+Demonstration code: [20622] Important !
+====================================== */
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[]) {
+    if (argc < 5 || argc > 6) {
+        printf("Type : exerc_4_1.exe engine_on parameter, gear_pos parameter, key_pos parameter, brake 2 parameter and brake1 parameter\n");
+    } else {
+        char engine_on = atoi(argv[1]);
+        char gear_pos = atoi(argv[2]);
+        char key_pos = atoi(argv[3]);
+        char brake1 = atoi(argv[4]);
+        char brake2 = atoi(argv[5]);
+        unsigned char b = (engine_on << 7) ^ (gear_pos << 4) ^ (key_pos << 2) ^ (brake2 << 1) ^ brake1;
+        
+        char output [8];
+        int a = 0;
+        for (int i = 7; 0 <= i; i--) {
+            output[a] = (b & (1 << i)) ? '1' : '0';
+            a++;
+        }
+        char* pointera = output;
+        char hexString[8];
+        int input = strtol(pointera, NULL, 2);
+        sprintf(hexString, "%X", input);
+        printf(hexString);
+    }   
+}
+    
---
+++ Submission/WP4/decode.c
@@ -0,0 +1,66 @@
+/* ====================================
+File name: decode.c 
+Date: 2019-02-20
+Group Number:07
+Members that contributed:
+Fredrik Ullman
+Mikael Köse Jansson
+Moritz Denke
+Demonstration code: [27290] Important !
+====================================== */
+#include <stdio.h>
+#include <stdlib.h>
+
+int getnum(char c);
+
+int main (int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Type : decode.exe HexadecimalValueOfChoice");
+    } else {
+        int left = getnum(argv[1][0]);
+        int right = getnum(argv[1][1]);
+        int final = ((left << 4) ^ right);
+
+        int engine_on = (final >> 7) & 0x01;
+        int gear_pos  = (final >> 4) & 0x07;
+        int key_pos   = (final >> 2) & 0x03;
+        int brake1    = (final >> 1) & 0x01;
+        int brake2    = (final)      & 0x01;
+
+        printf("Name          Value \n");
+        printf("------------------- \n");
+        printf("engine_on     %d \n", engine_on);
+        printf("gear_pos      %d \n", gear_pos);
+        printf("key_pos       %d \n", key_pos);
+        printf("brake1        %d \n", brake1);
+        printf("brake2        %d \n", brake2);
+        return 0;
+    }
+}
+
+int getnum(char c) {
+    switch (c) {
+    case 'A':
+    case 'a':
+        return 10;
+    case 'B':
+    case 'b':
+        return 11;
+    case 'C':
+    case 'c':
+        return 12;
+    case 'D':
+    case 'd':
+        return 13;
+    case 'E':
+    case 'e':
+        return 14;
+    case 'F':
+    case 'f':
+        return 15;
+    default:
+        return atoi(&c);
+    }
+}
+
+    
