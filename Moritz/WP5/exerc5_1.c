212121e5358cae501e0067e1a965a54c487068e2
DetailsMatterM <moritz.denke@outlook.com>
2020-02-24 16:37:38 +0100

    progress

--- Moritz/WP5/exerc5_1.c
+++ Moritz/WP5/exerc5_1.c
@@ -1,11 +1,20 @@
 const byte rows = 4;
 const byte columns = 4;
 const byte Output[rows] = {11, 10, 9, 8}; 
-const byte Input[columns] = {7, 6, 5, 4}; 
+const byte Input[columns] = {7, 6, 5, 4};
+
 bool pressed = false;
 byte pressedKey;
 
-void getKeypadInput() { 
+void setup() {
+    Serial.begin(9600);
+    for (byte i = 0; i < rows; i++) {
+        pinMode(Output[i], OUTPUT);
+    }
+  	attachInterrupt(digitalPinToInterrupt(2),press,FALLING);
+}
+
+void press(){
   for (byte i = 0; i < rows; i++) {
             digitalWrite(Output[i], LOW);
         } 
@@ -17,72 +26,20 @@ void getKeypadInput() {
                         for (byte i = 0; i < rows; i++) {
                             digitalWrite(Output[i], LOW);
                         }
+                        
                         pressedKey = r * 4 + c;
+                        pressed = true;
+                        return;
                     }
                 }
             }
         }
 }
 
-void setup() {
-    Serial.begin(9600);
-    for (byte i = 0; i < rows; i++) {
-        pinMode(Output[i], OUTPUT);
-    }
-  	attachInterrupt(digitalPinToInterrupt(2),press,FALLING);
-}
-
-void press(){
-  getKeypadInput();
-  pressed = true;
-}
-char toHex(byte num) {
-    switch (num) {
-    case 0:
-        return '0';
-    case 1:
-        return '1';
-    case 2:
-        return '2';
-    case 3:
-        return '3';
-    case 4:
-        return '4';
-    case 5:
-        return '5';
-    case 6:
-        return '6';
-    case 7:
-        return '7';
-    case 8:
-        return '8';
-    case 9:
-        return '9';
-    case 10:
-        return 'A';
-    case 11:
-        return 'B';
-    case 12:
-        return 'C';
-    case 13:
-        return 'D';
-    case 14:
-        return 'E';
-    case 15:
-        return 'F';
-    default:
-        return '?';
-    }
-}
-
-
-char last = '?';
 void loop() {
-  if(pressed) {
-    char printKey = toHex(pressedKey);
-    Serial.println(printKey);
-    last = printKey;
+  if (pressed) {
     pressed = false;
+    Serial.println(pressedKey, HEX);
   }
     
 }
