7e1765cefa83bd6fb3877abf96dff1dec1492643
DetailsMatterM <moritz.denke@outlook.com>
2020-03-03 15:36:08 +0100

    6_3

--- Moritz/WP6/exerc_6_3.c
+++ Moritz/WP6/exerc_6_3.c
@@ -2,16 +2,25 @@
 #define ML13_Control 0x0B00
 
 int objectWithinRange = 1;
-char control = ML13_Status;
+char status = ML13_Status;
+char control = ML13_Control;
+
 void main()
 {
-    
+    int failsafe = 0;
+    while(failsafe == 0) {
+        failsafe = doorLoop();
+    }
+    return 0;
 }
 
 int doorLoop() {
     char a = 1 ; 
     
-    if (control == 0x0B00 && objectWithinRange == 1) {
-        control = a << 1;
+    if (((control >> 2) & 0x01) == 0x0B01)  {
+        status = a << 1;
+        printf("Hello");
     }
+   // delay(10000);
+    return 1;
 }
