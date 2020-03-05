39bf98c861ff87397ab0a9b80b0ee738479e57e7
DetailsMatterM <moritz.denke@outlook.com>
2020-03-05 19:23:35 +0100

    6_5 not complete

--- Moritz/WP6/exerc_6_5.c
+++ Moritz/WP6/exerc_6_5.c
@@ -20,21 +20,22 @@ int outpos = 0;   // index for next character to be read ( fetch )
 int signal;
 
 void *put();
-//void *fetch();
+void *fetch();
 
 int main() {
   pthread_t putT;
-  //pthread_t fetchT;
+  pthread_t fetchT;
   pthread_attr_t attr;
 
   pthread_attr_init(&attr);
   pthread_create(&putT, &attr, put, NULL);
-  //pthread_create(&fetchT, &attr, fetch, NULL);
+  pthread_create(&fetchT, &attr, fetch, NULL);
 
   //int i;
 
   while (1) {
-    //printf("main is executing");
+    for (size_t i = 0; i < 10000; i++) {}; // do some work
+    printf("main is executing\n");
     
   }
 }
@@ -42,35 +43,70 @@ int main() {
 void *put() {
 
   while (1) {
-    int mute;
-    if ((mute = pthread_mutex_lock(&mutex)) == 0) {
-      pthread_cond_signal(&not_empty);
+    
+    if (pthread_mutex_lock(&count_mutex) == 0) {
+      if (count == MAX){
+        printf("Waiting for not full\n");
+        pthread_cond_wait(&not_full, &count_mutex);
+      }
+      pthread_mutex_unlock(&count_mutex);
+    }
+
+    if (pthread_mutex_lock(&mutex) == 0) {
       buffer[inpos] = letter;
-      printf("%c", letter);
+      
       letter++;
       if (letter > 'z') {
         letter = 'a';
       }
-      if(inpos == MAX - 1) {
+
+      if (inpos == MAX - 1) {
         inpos = 0;
       } else {
         inpos++;
       }
-      count++;
+
+      printf("Buffer stored\n");
+
       pthread_mutex_unlock(&mutex);
+    }
+
+    if (pthread_mutex_lock(&count_mutex) == 0) {
+      count++;
+      pthread_mutex_unlock(&count_mutex);
+      pthread_cond_signal(&not_empty);
     }
   }
 }
 
 void *fetch() {
   while (1) {
-      if (outpos < inpos) {
-        if(outpos == MAX - 1) {
-          outpos = 0;
-        } else {
-          outpos++;
-        }
-        count--;
+    
+    if (pthread_mutex_lock(&count_mutex) == 0) {
+      if (count == 0){
+        printf("Waiting for not empty\n");
+        pthread_cond_wait(&not_empty, &count_mutex);
       }
+      pthread_mutex_unlock(&count_mutex);
+    }
+
+    if (pthread_mutex_lock(&mutex) == 0) {
+      char output = buffer[outpos];
+      if(outpos == MAX - 1) {
+        outpos = 0;
+      } else {
+        outpos++;
+      }
+
+      printf("Buffer output %c\n", output);
+
+      pthread_mutex_unlock(&mutex);
+    }
+
+    if (pthread_mutex_lock(&count_mutex) == 0) {
+      count--;
+      pthread_mutex_unlock(&count_mutex);
+      pthread_cond_signal(&not_full);
+    }
   }
 }
