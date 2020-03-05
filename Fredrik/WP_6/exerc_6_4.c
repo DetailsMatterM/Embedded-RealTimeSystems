d789d884fe0ff957e2362c129eef9f144dd44599
Fredrik93 <fredrikullman93@gmail.com>
2020-03-05 09:11:02 +0100

    add time

--- Fredrik/WP_6/exerc_6_4.c
+++ Fredrik/WP_6/exerc_6_4.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-int program_time; // The global time, start value 0
+int program_time = 0; // The global time, start value 0
 
 void *time_count(void *param);
 void *read_inport(void *param);
@@ -10,18 +10,18 @@ double get_time_ms();
 
 int main()
 {
-    printf("%f\n", get_time_ms());
+
     pthread_t tid1;
     pthread_t tid2;
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    // Start up the thread time_count.
 
+    // Start up the thread time_count.
     pthread_create(&tid1, &attr, time_count, NULL);
     pthread_join(tid1, NULL);
-    // Start up the thread read_inport.
 
+    // Start up the thread read_inport.
     pthread_create(&tid2, &attr, read_inport, NULL);
     pthread_join(tid2, NULL);
 
@@ -33,16 +33,18 @@ int main()
 
 void *time_count(void *param)
 {
+    double originTime = get_time_ms();
+
     while (program_time < 50)
     {
-        printf("hello from time count\n");
-        get_time_ms();
-        program_time += 1;
 
+        printf(" origin time : %f time is : %f and program counter is at %d\n", originTime, get_time_ms(), program_time);
+
+        program_time += 1;
         // Check system-time ( get_time_ms())
         // Increase program_time by one every second.
     }
-    printf("%f time count \n", get_time_ms());
+    //  printf("%f time count \n", get_time_ms());
 
     pthread_exit(0);
 }
@@ -50,12 +52,12 @@ void *read_inport(void *param)
 {
     while (program_time < 50)
     {
-        printf("hello from read inport\n");
-
+        get_time_ms();
         program_time += 1;
         // Read Inport every 5 second.( Simulate
         //this just by print out a text : Reading Inport now)
     }
+    //  printf("%f read inport\n", get_time_ms());
     pthread_exit(0);
 }
 
