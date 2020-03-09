92989b264e8928817c7880464df4d6752fdd33c3
DetailsMatterM <moritz.denke@outlook.com>
2020-03-09 11:05:32 +0100

    corrections

--- Moritz/WP6/exerc_6_3.c
+++ Moritz/WP6/exerc_6_3.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define ML13_Status 0x0B10
-//00010000 or
 #define ML13_Control 0x0B00
 //these might be just the address we sent the instructions to
 
 int delay();
-char control = ML13_Control; //that should just be a char value we can save stuff in
-char status = ML13_Status;
+unsigned char control; //that should just be a char value we can save stuff in
+unsigned char status;
 void main()
 {
-
-    // if (status == 0x10 || control == 0b00000010)
-    // {
-    //     printf("Door is closing slowly.....\n");
-    //     status |= 0b100000000;
-    //     delay();
-    //     printf("Door is closed.\n");
-    //     status |= 0b00010000;
-    //     delay();
-    // }
-
-    //this checks if someone is approaching the sensor. if so, it changes status to
-    // "Door is wide open".
-
     char shiftInOne = 1;
     char shiftInZero = 0;
     //while loop runs continously keepig check on the door, we start with closed door
@@ -34,12 +19,12 @@ void main()
         {
             //shift to opening slowly
             status = shiftInOne << 6;
-            if (delay(5000) == 1) {
+            if (delayAndCheck(5000) == 1) {
                 //shift to wide open
                 // time door stays open
                 status = shiftInOne << 4;
                 status = shiftInZero << 6;
-                delay(10000); 
+                delayAndCheck(10000); 
             } // time it takes to open door
             
         }
@@ -48,27 +33,14 @@ void main()
         {
             status = shiftInZero << 4;
             status = shiftInOne << 7;
-            if (delay(5000) == 1) {
+            if (delayAndCheck(5000) == 1) {
                 status = shiftInOne << 5;
             }
         }
     }
-
-    // status = 0b0010100;
-    // if (status == 0b00010100)
-    // {
-    //     printf("Door is slowly opening ....\n");
-    //     status |= 0b100000000;
-    //     delay();
-    //     printf("Door is open\n");
-    //     status = 0b00001000;
-    //     delay();
-    // }
 }
 
-//rename the function to make sure that it is clear
-//that it does more than just waiting
-int delay(int length)
+int delayAndCheck(int length)
 {
     //use the gettimeofday and stire start time and when time has passed break
     for (int i = 0; i < length * 100; i++)
